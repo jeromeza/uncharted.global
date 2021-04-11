96e528bfc071390032e486f995213d556cfb6eef
jeromeza <jerome@sheed.co.za>
2021-04-11 15:41:55 +0200

    Update master_deploy.sh

--- master_deploy.sh
+++ master_deploy.sh
@@ -55,11 +55,10 @@ printf %"$COLUMNS"s |tr " " "-"
 echo "DEPLOYING WEBSERVER VIA HELM CHART - PLEASE WAIT..."
 printf %"$COLUMNS"s |tr " " "-"
 ./tooling/helm install webserver webserver_chart/ --kubeconfig ./kubeconfig-prod --set Image=nginx
-sleep 30
-export INGRESS_IP=$(./tooling/kubectl --kubeconfig ./kubeconfig-prod get ingress | tail -n1 | awk {'print $4'})
 
 printf %"$COLUMNS"s |tr " " "-"
 echo "YOUR WEBSITE SHOULD BE LIVE ON THE BELOW IP"
 printf %"$COLUMNS"s |tr " " "-"
-sleep 180
+sleep 120
+export INGRESS_IP=$(./tooling/kubectl --kubeconfig ./kubeconfig-prod get ingress | tail -n1 | awk {'print $4'})
 echo $INGRESS_IP

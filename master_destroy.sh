64e24d19966e7d7a0baaf7756886b4e906bab461
jeromeza <jerome@sheed.co.za>
2021-04-13 17:14:05 +0200

    Update master_destroy.sh

--- master_destroy.sh
+++ master_destroy.sh
@@ -5,5 +5,6 @@ if [ -z "$*" ]; then echo "Please specify a unique project name" && exit 0; fi
 # Remove helm chart and perform terraform destroy
 ./tooling/helm uninstall webserver --kubeconfig ./kubeconfig-prod
 ./tooling/helm uninstall prometheus-operator -n monitor --kubeconfig ./kubeconfig-prod
-sleep 120
+./tooling/kubectl --kubeconfig ./kubeconfig-prod delete -f prometheus_chart/templates/ingress.yml
+sleep 60
 printf 'yes' | ./tooling/terraform destroy -var project_id="$1"
